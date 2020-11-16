0e3db85408f49087a501ecee3667dff7d0dd7cc7
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2020-11-16 18:41:59 +0530

    webpack config changes.

--- avatar/webpack.config.js
+++ avatar/webpack.config.js
@@ -22,23 +22,9 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      // {
-      //   enforce: "pre",
-      //   test: /\.js$/,
-      //   loader: "source-map-loader",
-      // },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 8000,
-    stats: "minimal",
-  },
-  externals: {
-    react: "react",
+    extensions: [".js"],
   },
 };
